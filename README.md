74c5755277e505fbdc0f194ef6424abd0f982d7f
chirag-parmar <chiragparmar12209@gmail.com>
2017-10-21 00:46:22 +0530

    node js app for freeboard metrics

--- README.md
+++
@@ -1,3 +0,0 @@
-# MQTT LOGGER
-
-The MQTT Logger project aims to provide a simple solution to log incoming data into any mqtt channel on the broker. Currently, the supported languages are JavaScript and Python.
---
+++ dashboard.js
@@ -0,0 +1,109 @@
+var mqtt = require('mqtt')
+var fs = require('fs')
+const express        = require('express');
+const bodyParser     = require('body-parser');
+const app            = express();
+const os             = require('os');
+
+var port = process.env.PORT || 8000;
+var request = require('request')
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/', function(req,res) {
+  console.log(req.body)
+  metricData['CPU'] = os.cpus();
+  metricData['totalMem'] = os.totalmem();
+  metricData['freeMem'] = os.freemem();
+  res.send(JSON.stringify(metricData))
+})
+
+app.listen(port, () => {
+  console.log('We are live on ' + port);
+});
+
+
+var client = mqtt.connect('mqtt://127.0.0.1:1883')
+
+var metricTopics = ["$SYS/broker/uptime","$SYS/broker/load/bytes/sent/1min","$SYS/broker/load/bytes/received/1min",
+"$SYS/broker/clients/connected","$SYS/broker/load/messages/received/1min",
+"$SYS/broker/load/messages/sent/1min","#"]
+// DATABASE MANAGEMENT
+var metricData = {}
+metricData['bytesSent'] = ""
+metricData['bytesReceived'] = ""
+metricData['messagesSent'] = ""
+metricData['messagesReceived'] = ""
+metricData['lastMessage'] = []
+metricData['upTime'] = ""
+metricData['clientsConnected'] = ""
+metricData['CPU'] = 0
+metricData['totalMem'] = 0
+metricData['freeMem'] = 0
+
+//DATABASE MANAGEMENT
+
+client.on('connect', mqtt_connect);
+client.on('reconnect', mqtt_reconnect);
+client.on('error', mqtt_error);
+client.on('message', mqtt_messsageReceived);
+client.on('close', mqtt_close);
+
+function mqtt_connect() {
+    console.log("Connecting MQTT");
+    for (var i =0; i< metricTopics.length; i++){
+      client.subscribe(metricTopics[i], mqtt_subscribe);
+    }
+};
+
+function mqtt_subscribe(err, granted) {
+    console.log("Subscribed");
+    if (err) {console.log(err);}
+};
+
+function mqtt_reconnect(err) {
+    console.log("Reconnect MQTT");
+    if (err) {console.log(err);}
+	client = mqtt.connect('127.0.0.1:1883');
+};
+
+function mqtt_error(err) {
+    console.log("Error!");
+	if (err) {console.log(err);}
+};
+
+function after_publish() {
+	//do nothing
+};
+
+
+function mqtt_messsageReceived(topic, message, packet) {
+	var message_str = message.toString();
+	message_str = message_str.replace(/\n$/, '');
+  switch(topic) {
+    case "$SYS/broker/uptime":
+      metricData['upTime'] = message_str
+      break;
+    case "$SYS/broker/load/bytes/sent/1min":
+      metricData['bytesSent'] = message_str
+      break;
+    case "$SYS/broker/load/bytes/received/1min":
+      metricData['bytesReceived'] = message_str
+      break;
+    case "$SYS/broker/clients/connected":
+      metricData['clientsConnected'] = message_str
+      break;
+    case "$SYS/broker/load/messages/received/1min":
+      metricData['messagesReceived'] = message_str
+      break;
+    case "$SYS/broker/load/messages/sent/1min":
+      metricData['messagesSent'] = message_str
+      break;
+    default:
+      metricData['lastMessage'] = topic + " | " + message_str + " | " + Date().toISOString();
+  }
+};
+
+function mqtt_close() {
+	//console.log("Close MQTT");
+};
